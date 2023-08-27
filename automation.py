e444c0f322995e739554193dd2bc6aba5d5e686d
hr-meheraj <hrmeheraj2021@gmail.com>
2023-08-27 12:05:09 +0600

    Your commit message here

---
+++ automation.py
@@ -0,0 +1,15 @@
+# ✅ Convert miles to kilometers
+# // Calculate simple interest
+# // Find the largest number in a list
+# // Check a number is a prime number or not
+# // Sum of all digits in a number
+# // Reverse a string (Check palindrome)
+# // Calculate age
+# // Build a simple calculator
+
+
+# Covert Mile to Kilometer 
+    # one_miles = 1.60934 # kilometers
+    # userMileInput = int(input("Enter your Mile :"))
+    # print("Your {} miles is {} kilometers".format(userMileInput, userMileInput*one_miles))
+
