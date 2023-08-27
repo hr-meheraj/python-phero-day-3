3615e90ebdcc99afeafd967474656559f690e880
hr-meheraj <hrmeheraj2021@gmail.com>
2023-08-27 12:22:33 +0600

    solved simple interest

--- automation.py
+++ automation.py
@@ -1,5 +1,5 @@
 # ✅ Convert miles to kilometers
-# // Calculate simple interest
+# ✅ Calculate simple interest
 # // Find the largest number in a list
 # // Check a number is a prime number or not
 # // Sum of all digits in a number
@@ -8,8 +8,20 @@
 # // Build a simple calculator
 
 
+
 # Covert Mile to Kilometer 
     # one_miles = 1.60934 # kilometers
     # userMileInput = int(input("Enter your Mile :"))
     # print("Your {} miles is {} kilometers".format(userMileInput, userMileInput*one_miles))
 
+
+
+# // Calculate simple interest
+    # def simpleInterest() :
+    #     principal = float(input("Enter your principal : "))
+    #     times = float(input("Enter your times : "))
+    #     interestRate = float(input("Enter your interest Rate : "))
+    #     interest = (principal * times * interestRate)/100
+    #     print("Your interest is {}".format(interest))
+
+    # simpleInterest()
